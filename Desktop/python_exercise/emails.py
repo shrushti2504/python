31e5f852dd569e8da124def140fa85583cc23a7d
shrushti2504 <shrushtin.tagline@gmail.com>
2023-08-18 15:46:01 +0530

    emails changes

---
+++ Desktop/python_exercise/emails.py
@@ -0,0 +1,9 @@
+import re
+emails = ["abc@gmail.com", "123$tt*@xyz.com","good@bad@uk.in","nasa@usa12.space", "no-reply@domain.in", "ramhanuman@saketa.lok", "ruhi.mohan@exter123.123", "fake@fake123.fakercom"]
+emails2 = []
+for i in emails:
+    expression = r'^[\w\.-]+@+[\w\.-]+\.[a-zA-Z]{2,5}+$'
+    ans = re.match(expression,i)
+    if ans:
+        emails2.append(i)
+print(emails2)
